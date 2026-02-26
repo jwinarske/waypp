9cf31b32b246d5d7bc0e68ef179e7ee848f16a08
Joel Winarske <joel.winarske@linux.com>
2026-02-25 16:30:35 -0800

    vk-shadertoy segfault fix
    Signed-off-by: Joel Winarske <joel.winarske@linux.com>

--- examples/vk-shadertoy/app.cc
+++ examples/vk-shadertoy/app.cc
@@ -26,8 +26,7 @@
 #include <linux/input.h>
 
 void App::draw_frame(void* data, const uint32_t time) {
-  auto window = static_cast<Window*>(data);
-  auto shader_toy = static_cast<ShaderToy*>(window->get_user_data());
+  const auto shader_toy = static_cast<ShaderToy*>(data);
   shader_toy->draw_frame(time);
 }
 
@@ -42,8 +41,7 @@ App::App(const Configuration& config) : logging_(std::make_unique<Logging>()) {
 
   shader_toy_ = std::make_unique<ShaderToy>();
   wm_ = std::make_shared<XdgWindowManager>(display_, config.disable_cursor);
-  auto seat = wm_->get_seat();
-  if (seat.has_value()) {
+  if (const auto seat = wm_->get_seat(); seat.has_value()) {
     seat.value()->register_observer(this, this);
   }
 
@@ -84,14 +82,11 @@ void App::notify_seat_capabilities(Seat* seat,
                                    wl_seat* /* seat */,
                                    uint32_t /* caps */) {
   if (seat) {
-    auto keyboard = seat->get_keyboard();
-
-    if (keyboard.has_value()) {
+    if (const auto keyboard = seat->get_keyboard(); keyboard.has_value()) {
       keyboard.value()->register_observer(this, this);
     }
 
-    auto pointer = seat->get_pointer();
-    if (pointer.has_value()) {
+    if (const auto pointer = seat->get_pointer(); pointer.has_value()) {
       pointer.value()->register_observer(this, this);
     }
   }
@@ -128,8 +123,8 @@ void App::notify_keyboard_xkb_v1_key(Keyboard* keyboard,
                                      int xdg_key_symbol_count,
                                      const xkb_keysym_t* xdg_key_symbols) {
   if (xdg_key_symbol_count && state == KeyState::KEY_STATE_PRESS) {
-    auto app = static_cast<App*>(keyboard->get_user_data());
-    auto shader_toy = app->shader_toy_.get();
+    const auto app = static_cast<App*>(keyboard->get_user_data());
+    const auto shader_toy = app->shader_toy_.get();
 
     switch (xdg_key_symbols[0]) {
       case XKB_KEY_Escape:
@@ -157,6 +152,7 @@ void App::notify_keyboard_xkb_v1_key(Keyboard* keyboard,
         spdlog::info("Toggle fullscreen");
         app->toggle_fullscreen();
         break;
+      default:;
     }
   }
 }
@@ -180,9 +176,9 @@ void App::notify_pointer_motion(Pointer* pointer,
                                 uint32_t /* time */,
                                 double sx,
                                 double sy) {
-  auto app = static_cast<App*>(pointer->get_user_data());
-  auto shader_toy = app->shader_toy_.get();
-  auto os_window = shader_toy->get_app_os_window();
+  const auto app = static_cast<App*>(pointer->get_user_data());
+  const auto shader_toy = app->shader_toy_.get();
+  const auto os_window = shader_toy->get_app_os_window();
   os_window->app_data.iMouse[0] = sx;
   os_window->app_data.iMouse[1] = os_window->app_data.iResolution[1] - sy;
 }
@@ -193,9 +189,9 @@ void App::notify_pointer_button(Pointer* pointer,
                                 uint32_t /* time */,
                                 uint32_t button,
                                 uint32_t state) {
-  auto app = static_cast<App*>(pointer->get_user_data());
-  auto shader_toy = app->shader_toy_.get();
-  auto os_window = shader_toy->get_app_os_window();
+  const auto app = static_cast<App*>(pointer->get_user_data());
+  const auto shader_toy = app->shader_toy_.get();
+  const auto os_window = shader_toy->get_app_os_window();
 
   switch (button) {
     case BTN_LEFT:
